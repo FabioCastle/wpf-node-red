49e690c8d4ce2829c9ea1cd95793a6c38233b5f6
Castello Fabio <fabio.castello@celadagroup.com>
2025-09-02 12:07:26 +0200

    Fixed loading screen for Node-RED page

--- CastleWpf/NodeRed/NodeRedPage.xaml.cs
+++ CastleWpf/NodeRed/NodeRedPage.xaml.cs
@@ -2,39 +2,31 @@
 using System.Windows;
 using System.Windows.Controls;
 
-namespace CastleWpf.NodeRed
+namespace CastleWpf.NodeRed;
+
+/// <summary>
+/// Interaction logic for NodeRedPage.xaml
+/// </summary>
+public partial class NodeRedPage : Page
 {
-    /// <summary>
-    /// Interaction logic for NodeRedPage.xaml
-    /// </summary>
-    public partial class NodeRedPage : Page
+    public NodeRedPage()
     {
-        private readonly ManualResetEventSlim _viewLoaded = new(false);
+        InitializeComponent();
 
-        public NodeRedPage()
-        {
-            InitializeComponent();
+        Loaded += WaitForNodeRed;
+    }
 
-            Loaded += WaitForNodeRed;
-        }
+    private async void WaitForNodeRed(object sender, RoutedEventArgs e)
+    {
+        await Services.NodeRedManager.WaitForNodeReadReadyAsync();
 
-        private async void WaitForNodeRed(object sender, RoutedEventArgs e)
-        {
-            _viewLoaded.Reset();
+        NodeRedWebView.Source = new Uri("http://127.0.0.1:1880");
+        NodeRedWebView.NavigationCompleted += OnNavigationCompleted;
+    }
 
-            await Services.NodeRedManager.WaitForNodeReadReadyAsync();
-
-            NodeRedWebView.Source = new Uri("http://127.0.0.1:1880");
-            NodeRedWebView.NavigationCompleted += OnNavigationCompleted;
-
-            await Task.Run(_viewLoaded.Wait);
-            LoadingPanel.Visibility = Visibility.Collapsed;
-        }
-
-        private void OnNavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
-        {
-            _viewLoaded.Set();
-            NodeRedWebView.NavigationCompleted -= OnNavigationCompleted;
-        }
+    private void OnNavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+    {
+        LoadingPanel.Visibility = Visibility.Collapsed;
+        NodeRedWebView.NavigationCompleted -= OnNavigationCompleted;
     }
 }
